41335827cf30b2d355689637dd42fcff02e96541
karthiksbhat <krtk_bhat@yahoo.com>
2014-04-09 22:30:32 +0530

    small fixes

--- php/counter.php
+++ php/counter.php
@@ -3,10 +3,11 @@
 $hit_count = @file_get_contents('count.txt');
 $hit_count++;
 @file_put_contents('count.txt', $hit_count);
-header('Location: ../downloads/Jagrit-Bachpan.apk');
+header('Location: https://play.google.com/store/apps/details?id=org.jagriti.jagritbachpan');
 ?>
-<script type="text/javascript">
+<!--<script type="text/javascript">
 
   window.location.replace("http://projectjagriti.org");
   //window.location.replace("http://projectjagriti.org");
 </script>
+-->
